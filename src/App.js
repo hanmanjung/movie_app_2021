91dddd2fa3b914af44d63d70adfb8a8a67648e98
hanmanjung <201840233@email.daelim.ac.kr>
2021-10-13 16:54:34 +0900

    1013_commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,56 @@
-import React from "react";
-import axious from "axios";
 
-class App extends React.Component {
+import React from "react"
+import axios from "axios"
+import Movie from "./Movie"
+import "./App.css"
+
+class App extends React.Component{
   state = {
-    isLoading: true,
-    movies: []
+    isLoading : true,
+    movies : []
   }
 
-  getMovies = async () => {
-    const {
+  getMovies = async() => {
+    const{
       data: {
-        data: {movies}
+        data : {movies}
       }
-    } = await axios.get('https://yts.proxy.now.sh/list_movies.json')
-    const movies 
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating')
     console.log(movies);
+    this.setState({movies, isLoading: false})
   }
-  componentDidMount() {
-    this.getMovies()
-}
+
+  componentDidMount(){
+   this.getMovies()
+  }
   render() {
-    const {isLoading} = this.state
-    return (
-      <div>
-        { isLoading ? 'Loading...' : '영화 데이터 출력'}
-      </div>
+    const { isLoading, movies } = this.state
+    return(
+      <section className='container'>
+        { isLoading ?
+        ( <div className='loader'>
+          <span className='loader-text'> Loading....</span>
+          </div>
+          ) : (
+            <div className='movies'>
+              {
+                movies.map((movie) => {
+                  console.log(movie);
+                  return <Movie 
+                          key = {movie.id}
+                          id = {movie.id}
+                          year = {movie.year}
+                          title = {movie.title}
+                          summary = {movie.summary}
+                          poster = {movie.medium_cover_image}
+                          genres = {movie.genres}
+                          />
+                        })}
+            </div>
+          )}
+        </section>
     )
   }
 }
 
-export default App
+export default App;
